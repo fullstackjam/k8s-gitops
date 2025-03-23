854394f82997854b1da5cba57bd50b2d46ddca58
fullstackjam <fullstackjam@outlook.com>
2025-03-23 19:00:45 +0800

    Refactor Gitea configuration: remove hardcoded user details, add organization and repository structure

--- kubernetes/platform/gitea/files/config/main.go
+++ kubernetes/platform/gitea/files/config/main.go
@@ -1,5 +1,7 @@
 package main
 
+// TODO WIP clean this up
+
 import (
 	"log"
 	"os"
@@ -23,77 +25,48 @@ type Repository struct {
 	}
 }
 
-type User struct {
-	Name           string
-	FullName       string `yaml:"fullName"`
-	Email          string
-	TokenSecretRef string `yaml:"tokenSecretRef"`
-}
-
 type Config struct {
-	Users         []User         `yaml:"users"`
-	Organizations []Organization `yaml:"organizations"`
-	Repositories  []Repository   `yaml:"repositories"`
+	Organizations []Organization
+	Repositories  []Repository
 }
 
 func main() {
 	data, err := os.ReadFile("./config.yaml")
+
 	if err != nil {
 		log.Fatalf("Unable to read config file: %v", err)
 	}
 
 	config := Config{}
+
 	err = yaml.Unmarshal([]byte(data), &config)
+
 	if err != nil {
-		log.Fatalf("error parsing YAML: %v", err)
+		log.Fatalf("error: %v", err)
 	}
 
-	// 读取 Gitea 连接信息
-	giteaHost := os.Getenv("GITEA_HOST")
-	giteaUser := os.Getenv("GITEA_USER")
-	giteaPassword := os.Getenv("GITEA_PASSWORD")
+	gitea_host := os.Getenv("GITEA_HOST")
+	gitea_user := os.Getenv("GITEA_USER")
+	gitea_password := os.Getenv("GITEA_PASSWORD")
 
-	// 连接 Gitea
-	client, err := gitea.NewClient(giteaHost, gitea.SetBasicAuth(giteaUser, giteaPassword))
+	options := (gitea.SetBasicAuth(gitea_user, gitea_password))
+	client, err := gitea.NewClient(gitea_host, options)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, user := range config.Users {
-		existingUser, _, err := client.GetUserInfo(user.Name)
-		if err == nil && existingUser != nil {
-			log.Printf("User %s already exists, skipping creation.", user.Name)
-			continue
-		}
-
-		_, _, err = client.AdminCreateUser(gitea.CreateUserOption{
-			Username:           user.Name,
-			FullName:           user.FullName,
-			Email:              user.Email,
-			MustChangePassword: gitea.OptionalBool(false),
-			Password:           "change_me_later", // 这里可以随机生成密码，或者从 Secret 读取
-			SendNotify:         false,
-		})
-
-		if err != nil {
-			log.Printf("Failed to create user %s: %v", user.Name, err)
-		} else {
-			log.Printf("Created user %s successfully.", user.Name)
-		}
-	}
-
-	// 创建组织
 	for _, org := range config.Organizations {
 		_, _, err = client.CreateOrg(gitea.CreateOrgOption{
 			Name:        org.Name,
 			Description: org.Description,
 		})
+
 		if err != nil {
 			log.Printf("Create organization %s: %v", org.Name, err)
 		}
 	}
 
-	// 创建仓库或迁移仓库
 	for _, repo := range config.Repositories {
 		if repo.Migrate.Source != "" {
 			_, _, err = client.MigrateRepo(gitea.MigrateRepoOption{
@@ -105,17 +78,16 @@ func main() {
 				Private:        repo.Private,
 				MirrorInterval: "10m",
 			})
+
 			if err != nil {
 				log.Printf("Migrate %s/%s: %v", repo.Owner, repo.Name, err)
 			}
 		} else {
 			_, _, err = client.AdminCreateRepo(repo.Owner, gitea.CreateRepoOption{
-				Name:    repo.Name,
+				Name: repo.Name,
+				// Description: "TODO",
 				Private: repo.Private,
 			})
-			if err != nil {
-				log.Printf("Create repo %s/%s: %v", repo.Owner, repo.Name, err)
-			}
 		}
 	}
 }
