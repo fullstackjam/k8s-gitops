5a81bd3eb26b5eaf2e194323d07b183a5d715de2
fullstackjam <fullstackjam@outlook.com>
2025-09-25 19:47:52 +0800

    fix: update comments in external_test.go for clarity
    - Change references from "external module" to "terraform module" in comments
    - This improves clarity and consistency in the test file

--- test/external_test.go
+++ test/external_test.go
@@ -10,8 +10,8 @@ import (
 func TestTerraformExternal(t *testing.T) {
 	t.Parallel()
 
-	// Make a copy of the external module to a temporary directory. This allows running multiple tests in parallel
-	// against the same external module.
+	// Make a copy of the terraform module to a temporary directory. This allows running multiple tests in parallel
+	// against the same terraform module.
 	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../external", ".")
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
